a027069c7fb858791836a658d3faf34a8df1e4d8
Davi Medeiros <davi.medeiros@doodle.com>
2023-09-06 09:43:53 +0200

    build: configure hostname for next/image

--- next.config.js
+++ next.config.js
@@ -1,7 +1,12 @@
+const CONTENTFUL_IMAGES_DOMAIN = "images.ctfassets.net";
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   compiler: {
     styledComponents: true,
+  },
+  images: {
+    domains: [CONTENTFUL_IMAGES_DOMAIN],
   },
 };
 
