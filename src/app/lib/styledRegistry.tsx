2bcfe13aaa7d22e2008119704ce83e4e3762c5b8
Davi Medeiros <davi.medeiros@doodle.com>
2023-09-06 09:18:07 +0200

    build: set up styled components

---
+++ src/app/lib/styledRegistry.tsx
@@ -0,0 +1,28 @@
+"use client";
+
+import { useServerInsertedHTML } from "next/navigation";
+import React, { useState } from "react";
+import { ServerStyleSheet, StyleSheetManager } from "styled-components";
+
+export default function StyledComponentsRegistry({
+  children,
+}: React.PropsWithChildren) {
+  const [styledComponentsStyleSheet] = useState(() => new ServerStyleSheet());
+  const isServer = typeof window === "undefined";
+
+  useServerInsertedHTML(() => {
+    const styles = styledComponentsStyleSheet.getStyleElement();
+    styledComponentsStyleSheet.instance.clearTag();
+    return <>{styles}</>;
+  });
+
+  if (!isServer) {
+    return <>{children}</>;
+  }
+
+  return (
+    <StyleSheetManager sheet={styledComponentsStyleSheet.instance}>
+      {children}
+    </StyleSheetManager>
+  );
+}
