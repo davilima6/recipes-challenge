a32cbbc6720efb72f4603844aaec7138f38f8e02
Davi Medeiros <davi.medeiros@doodle.com>
2023-09-07 18:15:43 +0200

    refactor: extract formatRecipe out of useFormattedRecipe hook for better testability

--- src/app/lib/useFormattedRecipe.ts
+++ src/app/lib/useFormattedRecipe.ts
@@ -1,24 +1,19 @@
 import { useEffect, useState } from "react";
 
-import { markdownToHtml } from "./markdownToHtml";
 import { Recipe, RecipeDetails } from "./types";
+import { formatRecipe } from "./useFormattedRecipe.utils";
 
 export function useFormattedRecipe<R extends Recipe | RecipeDetails>(
   markdownRecipe: R
 ): R {
-  const [processedRecipe, setProcessedRecipe] = useState<R>(markdownRecipe);
+  const [recipe, setRecipe] = useState<R>(markdownRecipe);
 
   useEffect(() => {
-    async function formatRecipes() {
-      const htmlDescription = await markdownToHtml(
-        markdownRecipe.description
-      );
-      const formattedRecipe = { ...markdownRecipe };
-      formattedRecipe.description = htmlDescription;
-      setProcessedRecipe(formattedRecipe);
-    }
-    formatRecipes();
+    (async () => {
+      const formattedRecipe = await formatRecipe(markdownRecipe);
+      setRecipe(formattedRecipe);
+    })();
   }, [markdownRecipe]);
 
-  return processedRecipe;
+  return recipe;
 }
---
+++ src/app/lib/useFormattedRecipe.utils.ts
@@ -0,0 +1,14 @@
+import { markdownToHtml } from "./markdownToHtml";
+import { Recipe, RecipeDetails } from "./types";
+
+export async function formatRecipe<R extends Recipe | RecipeDetails>(
+  markdownRecipe: R
+): Promise<R> {
+  const formattedDescription = await markdownToHtml(markdownRecipe.description);
+  const formattedRecipe: R = {
+    ...markdownRecipe,
+    description: formattedDescription,
+  };
+
+  return formattedRecipe;
+}
